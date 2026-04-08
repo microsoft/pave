62ce5b8349ae65ff7f19ef4790adc5dfc44259e1
cpendery <cpendery@vt.edu>
2026-04-08 09:29:36 -0700

    build: update formula
    Signed-off-by: cpendery <cpendery@vt.edu>

--- Formula/pave.rb
+++ Formula/pave.rb
@@ -7,22 +7,22 @@ class Pave < Formula
   on_macos do
     on_arm do
       url "https://github.com/microsoft/pave/releases/download/v#{version}/pave-aarch64-apple-darwin.tar.gz"
-      sha256 "c74ffb814dfd24c392d9efccc7e3e2db57c4e63c879e8ac604f8deda7d3a0e42"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
     on_intel do
       url "https://github.com/microsoft/pave/releases/download/v#{version}/pave-x86_64-apple-darwin.tar.gz"
-      sha256 "0f8b4e33fa24ac43f568530d1a64c468b90c23b747a870b6018531df599f6b9a"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
    on_linux do
     on_arm do
       url "https://github.com/microsoft/pave/releases/download/v#{version}/pave-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "8d0529c4b9e39c86a1a7ec288ad841a4146f87924737b6347cb1f3251f42b518"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
     on_intel do
       url "https://github.com/microsoft/pave/releases/download/v#{version}/pave-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "dbb61af46a28d9a24091266725f070697d85b1623f08f18afafeea7a3c11c73f"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
